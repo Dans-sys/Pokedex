2ea60521d2df730232fde83264e6ec05b1e1d11e
Daniel Solano <daniel.solano@azucareralagrecia.com>
2024-01-31 08:58:13 -0600

    sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', (event) => {
         caches.open(CACHE_NAME)
             .then((cache) => {
                 return cache.addAll([
-                    '/',
+                    // '/',
                     '/index.html',
                     '/css/main.css',
                     '/js/main.js',
